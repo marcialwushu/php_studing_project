b59acc60f3f4679cd6c90152275cd56ab3cb70b3
marcialwushu <marcialwushu@gmail.com>
2016-12-07 15:18:30 -0200

    index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta ttp-equiv="Content-Type" content="text/html;charset="utf-8">
+<title>PHP teste logi</title>
+</head>
+
+<body>
+<form name="login" method="post" action="login.php">
+  <fieldset>
+    <legend>Digite dados de acesso</legend>
+    <label>Usuário</label>
+    <input type="text" name="usuario"/>
+
+    <label>Senha</label>
+    <input type="password" name="senha"/>
+
+    <input type="submit" name="entrar" value="Entrar"/>
+  </fieldset>
+</form>
+
+</body>
+</html>
